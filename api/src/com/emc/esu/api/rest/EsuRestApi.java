ea78332a20ba2245e6128324526b55271854051b
twincitiesguy@gmail.com <twincitiesguy@gmail.com@f9042120-e4f6-11de-b362-3513cf04e977>
2012-02-15 14:08:13 +0000

    removed illegal @Override and tweaked param docs on signRequest()
    git-svn-id: https://atmos-java.googlecode.com/svn/trunk/@70 f9042120-e4f6-11de-b362-3513cf04e977

--- api/src/com/emc/esu/api/rest/EsuRestApi.java
+++ api/src/com/emc/esu/api/rest/EsuRestApi.java
@@ -1840,8 +1840,7 @@ public class EsuRestApi extends AbstractEsuRestApi {
             throw new EsuException("Invalid URL", e);
         }
     }
-    
-	@Override
+
 	public ServiceInformation getServiceInformation() {
         try {
             String resource = context + "/service";
@@ -2103,9 +2102,9 @@ public class EsuRestApi extends AbstractEsuRestApi {
      * Generates the HMAC-SHA1 signature used to authenticate the request using
      * the Java security APIs.
      * 
-     * @param con the connection object
      * @param method the HTTP method used
-     * @param resource the resource path
+     * @param path the resource path
+     * @param query the URL querystring (if present)
      * @param headers the HTTP headers for the request
      * @throws IOException if character data cannot be encoded.
      * @throws GeneralSecurityException If errors occur generating the HMAC-SHA1
