89892205b8289e3275b27864e9c76e10f3513d06
Stu Arnett <Christopher.Arnett@emc.com>
2013-07-12 11:53:52 -0500

    Updated BucketFileAccess sample to illustrate sliding window of NFS access.

--- s3-sample/src/main/java/com/emc/vipr/sample/BucketFileAccess.java
+++ s3-sample/src/main/java/com/emc/vipr/sample/BucketFileAccess.java
@@ -1,12 +1,42 @@
 package com.emc.vipr.sample;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.TimeoutException;
 
 import com.amazonaws.util.StringInputStream;
 import com.emc.vipr.services.s3.ViPRS3Client;
 import com.emc.vipr.services.s3.model.*;
+import com.emc.vipr.services.s3.model.Object;
 
+/**
+ * EMC ViPR data services includes an option to export S3 buckets and Swift
+ * containers over NFS.  The set of objects exported in this manor is fixed and new
+ * objects added via the REST protocol will not be visible in existing exports.
+ * Conversely, you cannot create or delete objects in the exported mount points,
+ * although when mounted read-write, you can modify existing objects.  While
+ * objects are available over NFS, they are inaccessible via REST and vice versa.
+ * <p/>
+ * The set of objects to be exported is defined by start-token and end-token parameters, which
+ * represent specific points in time for the bucket (bucket versions). These
+ * tokens are returned from any call ("get" or "set") and one token may be used in subsequent
+ * "set" calls with the following outcome:
+ * <p/>
+ * when setting mode to readOnly or readWrite and a token is included,
+ * only enable access for objects *newer* than the token.
+ * <p/>
+ * when setting mode to disabled and a token is included, only disable
+ * access for objects *older* than the token.
+ * <p/>
+ * note that the tokens (bucket versions) are inclusive, so it is possible that start-token = end-token (this will
+ * likely include only one object).
+ * <p/>
+ * Leaving out the token in a request signifies that all objects in the bucket should be
+ * affected by the operation.
+ * <p/>
+ * Using the "set" call to enable access for new objects and disable access for
+ * old objects, you can effectively establish a sliding window (over time) of
+ * objects available via NFS for a given bucket.
+ */
 public class BucketFileAccess {
     private ViPRS3Client s3;
 
@@ -20,12 +50,15 @@ public class BucketFileAccess {
         String key2 = "test2.txt";
         String key3 = "test3.txt";
         String key4 = "test4.txt";
+        String key5 = "test5.txt";
+        String key6 = "test6.txt";
         String content = "Hello World!";
         String clientHost = "10.10.10.10"; // change to a real client to test
         String clientUid = "501"; // change to your client uid to test
         long fileAccessDuration = 20; // seconds
 
         try {
+            // create some objects
             s3.createBucket(bucketName);
             s3.putObject(bucketName, key1, new StringInputStream(content), null);
             s3.putObject(bucketName, key2, new StringInputStream(content), null);
@@ -34,114 +67,140 @@ public class BucketFileAccess {
 
             SetBucketFileAccessModeRequest request = new SetBucketFileAccessModeRequest();
             request.setBucketName(bucketName);
-            request.setAccessMode(ViPRConstants.FileAccessMode.readOnly);
+            request.setAccessMode(ViPRConstants.FileAccessMode.readWrite);
             request.setDuration(fileAccessDuration);
             request.setHostList(Arrays.asList(clientHost));
             request.setUid(clientUid);
 
-            // change mode to read-only
-            SampleUtils.log(
-                    "Changing access mode on bucket %s to NFS read-only",
-                    bucketName);
-            BucketFileAccessModeResult result = s3
-                    .setBucketFileAccessMode(request);
+            // switch to NFS access for current objects the bucket
+            SampleUtils.log("Enabling NFS access (workflow A)");
+            BucketFileAccessModeResult result = s3.setBucketFileAccessMode(request);
 
-            // this token represents a point in time before which any objects that were created will be available via
-            // NFS. Objects created since this time will *not* be available via NFS unless a subsequent call is made to
-            // enable them. For example, say you have a bucket workflow in which one process requires filesystem access
-            // and after that process is complete, the objects should once again be available via REST. This token
-            // allows you to execute your workflow in batches. You can turn on filesystem access using the previous
-            // token, which will enable it only for new objects. Once that access expires, those objects will be
-            // available again via REST. The next batch will use the token returned from this batch and so on.
-            String token = result.getEndToken();
-            SampleUtils.log("Token to represent net-new objects: %s", token);
+            // the token is the bucket version. it represents a point in time. objects older than this token are now
+            // exported via NFS. any objects created after this point in time will not be accessible via NFS, but *will*
+            // be accessible via REST.
+            String tokenA = result.getEndToken();
+            SampleUtils.log("Token: %s", tokenA);
 
             // wait until complete (change is asynchronous)
             SampleUtils.log("Waiting for bucket mode to change...");
-            waitForTransition(bucketName,
-                    ViPRConstants.FileAccessMode.readOnly, 5000);
-            SampleUtils
-                    .log("Change complete; bucket is now in NFS read-only mode");
+            waitForTransition(bucketName, ViPRConstants.FileAccessMode.readWrite, 30000);
+            SampleUtils.log("Change complete; bucket is now in NFS read-write mode");
 
-            // now the bucket should be accessible via NFS, let's get the
-            // details
+            // now the bucket should be accessible via NFS, let's get the details
             GetFileAccessRequest fileAccessRequest = new GetFileAccessRequest();
             fileAccessRequest.setBucketName(bucketName);
+            GetFileAccessResult fileAccessResult = s3.getFileAccess(fileAccessRequest);
 
-            GetFileAccessResult fileAccessResult;
-            int waitMs = 250;
-
-            // objects are asynchronously prepared and returned in the list as
-            // they become available. this block will iterate until we have
-            // paths to all objects in the bucket.
-            do {
-                fileAccessResult = s3.getFileAccess(fileAccessRequest);
-
-                // here are your mount points for each object
-                String mountPoint = fileAccessResult.getMountPoints().get(0);
-                SampleUtils.log("NFS mount points for objects:");
-                for (com.emc.vipr.services.s3.model.Object object : fileAccessResult
-                        .getObjects()) {
-                    SampleUtils.log("    %s:%s", mountPoint,
-                            object.getRelativePath());
+            // here are your mount points and objects
+            Map<String, List<com.emc.vipr.services.s3.model.Object>> mountMap
+                    = new HashMap<String, List<com.emc.vipr.services.s3.model.Object>>();
+            for (com.emc.vipr.services.s3.model.Object object : fileAccessResult.getObjects()) {
+                List<com.emc.vipr.services.s3.model.Object> objects = mountMap.get(object.getDeviceExport());
+                if (objects == null) {
+                    objects = new ArrayList<com.emc.vipr.services.s3.model.Object>();
+                    mountMap.put(object.getDeviceExport(), objects);
                 }
+                objects.add(object);
+            }
 
-                if (fileAccessResult.isTruncated()) {
-                    SampleUtils
-                            .log("list of objects was truncated; will try for more objects in %dms",
-                                    waitMs);
-                    Thread.sleep(waitMs);
-                    fileAccessRequest.setMarker(fileAccessResult.getLastKey());
+            SampleUtils.log("NFS mount points and objects (workflow A):");
+            for (String export : mountMap.keySet()) {
+                SampleUtils.log("> %s", export);
+                for (Object object : mountMap.get(export)) {
+                    SampleUtils.log("> > %s => %s", object.getName(), object.getRelativePath());
                 }
-            } while (fileAccessResult.isTruncated());
+            }
 
-            // change mode back to disabled (must do this before going to
-            // read-write)
-            SampleUtils.log("Changing file access mode back to disabled");
-            request = new SetBucketFileAccessModeRequest();
-            request.setBucketName(bucketName);
-            request.setAccessMode(ViPRConstants.FileAccessMode.disabled);
-            s3.setBucketFileAccessMode(request);
+            // create more objects (part of a new workflow in the bucket)
+            s3.putObject(bucketName, key5, new StringInputStream(content), null);
+            s3.putObject(bucketName, key6, new StringInputStream(content), null);
 
-            // wait until complete
-            SampleUtils.log("Waiting for bucket mode to change...");
-            waitForTransition(bucketName,
-                    ViPRConstants.FileAccessMode.disabled, 5000);
-            SampleUtils
-                    .log("Change complete; bucket file access is now disabled");
-
-            // now change to read-write
-            SampleUtils.log("Changing file access mode to read-write");
+            // now, switch to NFS access for the new objects using the end-token returned from the last call.
+            // when enabling NFS access, all objects *newer* than the token are included in the NFS export.
+            SampleUtils.log("Enabling NFS access (workflow B)");
             request = new SetBucketFileAccessModeRequest();
             request.setBucketName(bucketName);
             request.setAccessMode(ViPRConstants.FileAccessMode.readWrite);
+            request.setDuration(fileAccessDuration);
+            request.setHostList(Arrays.asList(clientHost));
+            request.setUid(clientUid);
+            request.setToken(tokenA);
             s3.setBucketFileAccessMode(request);
 
+            String tokenB = result.getEndToken();
+            SampleUtils.log("End-token for workflow B: %s", tokenB);
+
+            // wait until complete (change is asynchronous)
             SampleUtils.log("Waiting for bucket mode to change...");
-            // wait until complete
-            waitForTransition(bucketName,
-                    ViPRConstants.FileAccessMode.readWrite, 5000);
-            SampleUtils
-                    .log("Change complete; bucket file access is now NFS read-write");
+            waitForTransition(bucketName, ViPRConstants.FileAccessMode.readWrite, 30000);
+            SampleUtils.log("Change complete; bucket is now in NFS read-write mode");
 
-            // now the objects are writable via NFS
+            // now the bucket should be accessible via NFS, let's get the details
+            fileAccessRequest = new GetFileAccessRequest();
+            fileAccessRequest.setBucketName(bucketName);
+            fileAccessResult = s3.getFileAccess(fileAccessRequest);
 
-            // change mode back to disabled
-            SampleUtils.log("Changing file access mode back to disabled");
+            // here are your mount points and objects
+            mountMap = new HashMap<String, List<com.emc.vipr.services.s3.model.Object>>();
+            for (com.emc.vipr.services.s3.model.Object object : fileAccessResult.getObjects()) {
+                List<com.emc.vipr.services.s3.model.Object> objects = mountMap.get(object.getDeviceExport());
+                if (objects == null) {
+                    objects = new ArrayList<com.emc.vipr.services.s3.model.Object>();
+                    mountMap.put(object.getDeviceExport(), objects);
+                }
+                objects.add(object);
+            }
+
+            SampleUtils.log("NFS mount points and objects (workflow B):");
+            for (String export : mountMap.keySet()) {
+                SampleUtils.log("> %s", export);
+                for (Object object : mountMap.get(export)) {
+                    SampleUtils.log("> > %s => %s", object.getName(), object.getRelativePath());
+                }
+            }
+
+            // NFS access for a bucket is enabled only for objects between a starting and ending bucket version
+            // (start-token and end-token). at this point in the sample, there are two simultaneous sequential NFS
+            // "workflows" for the same bucket. you can have as many of these workflows as you want so long as they are
+            // sequential and are therefore disabled in the same order they were enabled (there is only one sliding
+            // window of access).
+
+            // now let's assume the first workflow is complete.
+            // switch mode back to disabled for those objects by passing end-token from first workflow.
+            // this makes all objects in first workflow available via REST again.
+            // when disabling NFS access, all objects *older* than the token are excluded from the NFS export.
+            SampleUtils.log("Work complete, changing file access mode back to disabled (workflow A):");
             request = new SetBucketFileAccessModeRequest();
             request.setBucketName(bucketName);
             request.setAccessMode(ViPRConstants.FileAccessMode.disabled);
+            request.setToken(tokenA);
             s3.setBucketFileAccessMode(request);
 
             // wait until complete
             SampleUtils.log("Waiting for bucket mode to change...");
-            waitForTransition(bucketName,
-                    ViPRConstants.FileAccessMode.disabled, 5000);
-            SampleUtils
-                    .log("Change complete; bucket file access is now disabled");
+            waitForTransition(bucketName, ViPRConstants.FileAccessMode.disabled, 30000);
+            SampleUtils.log("Change complete; bucket file access is now disabled");
+
+            // now the second workflow is complete.
+            // switch mode back to disabled for these objects by passing end-token from second workflow.
+            // NOTE: setting mode to disabled *without* specifying a token will disable NFS access for *all* objects in
+            // the bucket.
+            SampleUtils.log("Work complete, changing file access mode back to disabled (workflow B):");
+            request = new SetBucketFileAccessModeRequest();
+            request.setBucketName(bucketName);
+            request.setAccessMode(ViPRConstants.FileAccessMode.disabled);
+            request.setToken(tokenB);
+            s3.setBucketFileAccessMode(request);
+
+            // wait until complete
+            SampleUtils.log("Waiting for bucket mode to change...");
+            waitForTransition(bucketName, ViPRConstants.FileAccessMode.disabled, 30000);
+            SampleUtils.log("Change complete; bucket file access is now disabled");
         } finally {
             SampleUtils.cleanBucket(s3, bucketName);
         }
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -150,44 +209,33 @@ public class BucketFileAccess {
     }
 
     /**
-     * waits until the target access mode is completely transitioned on the
-     * specified bucket.
+     * waits until the target access mode is completely transitioned on the specified bucket.
      *
      * @param bucketName bucket name
-     * @param targetMode target access mode to wait for (readOnly, readWrite, or
-     *                   disabled)
-     * @param timeout    after the specified number of milliseconds, this method will
-     *                   throw a TimeoutException
+     * @param targetMode target access mode to wait for (readOnly, readWrite, or disabled)
+     * @param timeout    after the specified number of milliseconds, this method will throw a TimeoutException
      * @throws InterruptedException if interrupted while sleeping between GET intervals
-     * @throws TimeoutException     if the specified timeout is reached before transition is
-     *                              complete
+     * @throws TimeoutException     if the specified timeout is reached before transition is complete
      */
-    protected void waitForTransition(String bucketName,
-                                     ViPRConstants.FileAccessMode targetMode, int timeout)
+    protected void waitForTransition(String bucketName, ViPRConstants.FileAccessMode targetMode, int timeout)
             throws InterruptedException, TimeoutException {
         long start = System.currentTimeMillis(), interval = 500;
         while (true) {
             // GET the current access mode
-            BucketFileAccessModeResult result = s3
-                    .getBucketFileAccessMode(bucketName);
-            if (targetMode == result.getAccessMode())
-                return; // transition is complete
+            BucketFileAccessModeResult result = s3.getBucketFileAccessMode(bucketName);
+            if (targetMode == result.getAccessMode()) return; // transition is complete
 
             if (targetMode.isTransitionState())
-                throw new IllegalArgumentException("Invalid target mode: "
-                        + targetMode);
+                throw new IllegalArgumentException("Invalid target mode: " + targetMode);
 
-            if (!result.getAccessMode().isTransitionState()
-                    || !result.getAccessMode().transitionsToTarget(targetMode))
-                throw new RuntimeException(String.format(
-                        "Bucket %s in mode %s will never get to mode %s",
+            if (!result.getAccessMode().isTransitionState() || !result.getAccessMode().transitionsToTarget(targetMode))
+                throw new RuntimeException(String.format("Bucket %s in mode %s will never get to mode %s",
                         bucketName, result.getAccessMode(), targetMode));
 
             // if we've reached our timeout
             long runTime = System.currentTimeMillis() - start;
             if (runTime >= timeout)
-                throw new TimeoutException(String.format(
-                        "Access mode transition for %s took longer than %dms",
+                throw new TimeoutException(String.format("Access mode transition for %s took longer than %dms",
                         bucketName, timeout));
 
             // transitioning; wait and query again
--- s3-sample/src/main/java/com/emc/vipr/sample/S3ClientFactory.java
+++ s3-sample/src/main/java/com/emc/vipr/sample/S3ClientFactory.java
@@ -1,5 +1,7 @@
 package com.emc.vipr.sample;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +16,8 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Properties;
 
+import com.emc.vipr.services.s3.NamespaceRequestHandler;
+import com.emc.vipr.services.s3.ViPRS3Client;
 import org.apache.commons.codec.binary.Base64;
 
 import com.amazonaws.ClientConfiguration;
@@ -21,8 +25,6 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3EncryptionClient;
 import com.amazonaws.services.s3.model.EncryptionMaterials;
-import com.emc.vipr.services.s3.NamespaceRequestHandler;
-import com.emc.vipr.services.s3.ViPRS3Client;
 
 /**
  * This class looks on the classpath for a file named viprs3.properties and uses it to
@@ -35,12 +37,12 @@ import com.emc.vipr.services.s3.ViPRS3Client;
  * <dl>vipr.namespace</dl><dd>(Optional) The ViPR namespace to connect to.  Generally
  * this is required if your endpoint is an IP or not in the format of {namespace}.company.com</dd>
  * </dt>
- * @author cwikj
  *
+ * @author cwikj
  */
 public class S3ClientFactory {
     public static final String VIPR_PROPERTIES_FILE = "vipr.properties";
-    
+
     public static final String PROP_ACCESS_KEY_ID = "vipr.access_key_id";
     public static final String PROP_SECRET_KEY = "vipr.secret_key";
     public static final String PROP_ENDPOINT = "vipr.endpoint";
@@ -49,65 +51,82 @@ public class S3ClientFactory {
     public static final String PROP_PRIVATE_KEY = "vipr.encryption.privatekey";
     public static final String PROP_PROXY_HOST = "vipr.proxy.host";
     public static final String PROP_PROXY_PORT = "vipr.proxy.port";
-    
+
+    /**
+     * Locates and loads the properties file for the test configuration.  This file can
+     * reside in one of two places: somewhere in the CLASSPATH or in the user's home
+     * directory.
+     *
+     * @return the contents of the properties file as a {@link Properties} object.
+     * @throws FileNotFoundException if the file was not found
+     * @throws IOException           if there was an error reading the file.
+     */
     public static Properties getProperties() throws FileNotFoundException, IOException {
         InputStream in = S3ClientFactory.class.getClassLoader().getResourceAsStream(VIPR_PROPERTIES_FILE);
-        if(in == null) {
-            throw new FileNotFoundException(VIPR_PROPERTIES_FILE);
+        if (in == null) {
+            // Check in home directory
+            File homeProps = new File(System.getProperty("user.home") + File.separator +
+                    VIPR_PROPERTIES_FILE);
+            if (homeProps.exists()) {
+                in = new FileInputStream(homeProps);
+            } else {
+                throw new FileNotFoundException(VIPR_PROPERTIES_FILE);
+            }
         }
-        
+
         Properties props = new Properties();
         props.load(in);
         in.close();
-        
+
         return props;
     }
-    
+
     public static ViPRS3Client getS3Client() {
         try {
             Properties props = getProperties();
-            
+
             String accessKey = getPropertyNotEmpty(props, PROP_ACCESS_KEY_ID);
             String secretKey = getPropertyNotEmpty(props, PROP_SECRET_KEY);
             String endpoint = getPropertyNotEmpty(props, PROP_ENDPOINT);
-            
+
             BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
             ViPRS3Client client = new ViPRS3Client(creds);
             client.setEndpoint(endpoint);
-                        
+
             String namespace = props.getProperty(PROP_NAMESPACE);
-            if(namespace != null) {
-               client.setNamespace(namespace);
+            if (namespace != null) {
+                client.setNamespace(namespace);
             }
             checkProxyConfig(client, props);
-            
+
             return client;
         } catch (IOException e) {
             throw new RuntimeException("Could not load properties file", e);
         }
     }
-    
+
     /**
      * Creates an EncryptionClient for testing.  Loads the public and private keys from
      * the properties file (not suitable for production).
+     *
      * @return
      * @throws IOException
      */
     public static AmazonS3EncryptionClient getEncryptionClient() throws IOException {
         Properties props = getProperties();
-        
+
         String accessKey = getPropertyNotEmpty(props, PROP_ACCESS_KEY_ID);
         String secretKey = getPropertyNotEmpty(props, PROP_SECRET_KEY);
         String endpoint = getPropertyNotEmpty(props, PROP_ENDPOINT);
         String publicKey = getPropertyNotEmpty(props, PROP_PUBLIC_KEY);
         String privateKey = getPropertyNotEmpty(props, PROP_PRIVATE_KEY);
-        
+
         byte[] pubKeyBytes = Base64.decodeBase64(publicKey.getBytes("US-ASCII"));
         byte[] privKeyBytes = Base64.decodeBase64(privateKey.getBytes("US-ASCII"));
-        
+
         X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(pubKeyBytes);
         PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(privKeyBytes);
-        
+
         PublicKey pubKey;
         PrivateKey privKey;
         try {
@@ -117,60 +136,60 @@ public class S3ClientFactory {
         } catch (GeneralSecurityException e) {
             throw new RuntimeException("Could not load key pair: " + e, e);
         }
-        
+
         EncryptionMaterials keys = new EncryptionMaterials(new KeyPair(pubKey, privKey));
-        
+
         BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, secretKey);
         AmazonS3EncryptionClient client = new AmazonS3EncryptionClient(creds, keys);
         client.setEndpoint(endpoint);
-        
+
         String namespace = props.getProperty(PROP_NAMESPACE);
-        if(namespace != null) {
-           NamespaceRequestHandler handler = new NamespaceRequestHandler(namespace);
-           client.addRequestHandler(handler);
+        if (namespace != null) {
+            NamespaceRequestHandler handler = new NamespaceRequestHandler(namespace);
+            client.addRequestHandler(handler);
         }
-        
+
         checkProxyConfig(client, props);
-        
+
         return client;
     }
-    
+
     private static void checkProxyConfig(AmazonS3Client client, Properties props) {
         String proxyHost = props.getProperty(PROP_PROXY_HOST);
-        if(proxyHost != null && !proxyHost.isEmpty()) {
+        if (proxyHost != null && !proxyHost.isEmpty()) {
             int proxyPort = Integer.parseInt(props.getProperty(PROP_PROXY_PORT));
             ClientConfiguration config = new ClientConfiguration();
             config.setProxyHost(proxyHost);
             config.setProxyPort(proxyPort);
             client.setConfiguration(config);
-        }        
+        }
     }
-    
-    static String getPropertyNotEmpty(Properties p, String key) {
+
+    private static String getPropertyNotEmpty(Properties p, String key) {
         String value = p.getProperty(key);
-        if(value == null || value.isEmpty()) {
+        if (value == null || value.isEmpty()) {
             throw new RuntimeException(String.format("The property %s is required", key));
         }
         return value;
     }
-    
+
     // Generates a RSA key pair for testing.
     public static void main(String[] args) {
         try {
             KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
             keyGenerator.initialize(1024, new SecureRandom());
             KeyPair myKeyPair = keyGenerator.generateKeyPair();
-            
+
             // Serialize.
             byte[] pubKeyBytes = myKeyPair.getPublic().getEncoded();
             byte[] privKeyBytes = myKeyPair.getPrivate().getEncoded();
-            
+
             String pubKeyStr = new String(Base64.encodeBase64(pubKeyBytes, false), "US-ASCII");
             String privKeyStr = new String(Base64.encodeBase64(privKeyBytes, false), "US-ASCII");
-            
+
             System.out.println("Public Key: " + pubKeyStr);
             System.out.println("Private Key: " + privKeyStr);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
