4d7044810b60abc9e1cba0f870184edd5c57bf56
arnett, stu <stu.arnett@emc.com>
2018-05-11 14:03:36 -0500

    Added ETag as a property of ObjectMetadata and BasicResponse, and implemented workaround for Github Issue #3

--- src/main/java/com/emc/atmos/api/bean/BasicResponse.java
+++ src/main/java/com/emc/atmos/api/bean/BasicResponse.java
@@ -1,28 +1,34 @@
 /*
- * Copyright (c) 2013-2016, EMC Corporation.
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * BSD 3-Clause License
  *
- * + Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * + Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- * + The name of EMC Corporation may not be used to endorse or promote
- *   products derived from this software without specific prior written
- *   permission.
+ * Copyright (c) 2013-2018, Dell EMC
+ * All rights reserved.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 package com.emc.atmos.api.bean;
 
@@ -44,6 +50,7 @@ public class BasicResponse {
     protected String location;
     protected Date lastModified;
     protected Date date;
+    protected String eTag;
 
     public Date getDate() {
         return date;
@@ -107,6 +114,19 @@ public class BasicResponse {
 
     public void setLastModified( Date lastModified ) {
         this.lastModified = lastModified;
+    }
+
+    /**
+     * Returns the ETag of the response. If the request is for an object and that object was written in one POST/PUT
+     * operation, this will be the MD5 checksum of the data.
+     * Note: this feature is only available on ECS 3.0+
+     */
+    public String getETag() {
+        return eTag;
+    }
+
+    public void setETag( String eTag ) {
+        this.eTag = eTag;
     }
 
     protected String getFirstHeader( String headerName ) {
